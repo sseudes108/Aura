6a11b1fff0e4461bbe1c264f7cf4b06b1ee8322a
Eudes <eudes.jss92@gmail.com>
2024-09-29 18:36:21 -0300

    Start UI Widgets - AuraV1

---
+++ Aura_V1/Source/Aura_V1/Public/UI/WidgetController/AuraWidgetController.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "UObject/NoExportTypes.h"
+#include "AuraWidgetController.generated.h"
+
+class UAbilitySystemComponent;
+class UAttributeSet;
+
+UCLASS()
+class AURA_V1_API UAuraWidgetController : public UObject{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
+	TObjectPtr<APlayerController> PlayerController;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
+	TObjectPtr<APlayerState> PlayerState;
+
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "WidgetController")
+	TObjectPtr<UAttributeSet> AttributeSet;
+};
