7c815357b624ccd4acbfe26b87654a55e8ec2cb9
Eudes <eudes.jss92@gmail.com>
2024-10-08 22:46:09 -0300

    Create HUD and progress Bars - AuraV2

---
+++ Aura_V2/Source/Aura_V2/Public/UI/HUD/AuraHUD.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "AuraHUD.generated.h"
+
+class UAuraUserWidget;
+
+UCLASS()
+class AURA_V2_API AAuraHUD : public AHUD{
+	GENERATED_BODY()
+	
+protected:
+	virtual void BeginPlay() override;
+	
+public:
+	UPROPERTY()
+	TObjectPtr<UAuraUserWidget> OverlayWidget;
+	
+private:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UAuraUserWidget> OverlayWidgetClass;
+};
